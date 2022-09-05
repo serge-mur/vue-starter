4ecc0887e2b4a776e886f94aaa5a261cf97bc91e
serge <serge-mur@yandex.ru>
2022-09-05 15:14:24 +0300

    add vuex sample

--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,28 @@
 import { createStore } from 'vuex'
 
 export default createStore({
-  state: {
-  },
-  getters: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
-  modules: {
-  }
+	state: {
+		count: 1
+	},
+	getters: {
+		// count(state) {
+		// 	return state.count
+		// },
+		// count: state => state.count,
+		x2(state) {
+			return state.count * 2
+		}
+	},
+	mutations: {
+		increment(state) {
+			state.count++
+		}
+	},
+	actions: {
+		increment(store) {
+			store.commit('increment')
+		}
+	},
+	modules: {
+	}
 })
--- src/views/VuexTestView.vue
+++ src/views/VuexTestView.vue
@@ -1,5 +1,33 @@
 <template>
     <div class="home">
-      <h1>This is an vuex page</h1>
+        <h1>This is an vuex page</h1>
+
+        <p>counter: {{ count }}</p>
+        <p>counter*2: {{ x2 }}</p>
+
+        <!-- <button @click="$store.commit('increment')">count +1</button> -->
+        <button @click="increment">count +1</button>
+
     </div>
 </template>
+<script>
+import { mapState, mapGetters, mapActions } from 'vuex';
+
+export default {
+    computed: {
+        ...mapState(['count']),
+        ...mapGetters(['x2'])
+
+        // count() {
+        //     return this.$store.state.count
+        // },
+        // x2() {
+        //     return this.$store.getters.x2
+        // }
+
+    },
+    methods: {
+        ...mapActions(['increment'])
+    }
+}
+</script>
